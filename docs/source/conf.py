98aadf95aa8bae6d438729ba68a951365860191a
José Rodrigues <43458593+astrojose9@users.noreply.github.com>
2021-12-27 15:53:36 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,8 +14,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('/home/jrodrigues/Documents/PhD/fulmar'))
 
-import furo
-
 # -- Project information -----------------------------------------------------
 
 project = 'FULMAR'
